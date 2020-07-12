900ab68499c022a74a5959d623e3353921e44218
Nati <natalia.pupushi@gmail.com>
2020-07-12 15:53:13 -0300

    FAQ's page & api data implemented

---
+++ js/APIcalls.js
@@ -0,0 +1,23 @@
+
+async function getImageRandomDog() {
+
+    const section = document.querySelector(".dog-list");
+
+    try {
+        for (let i = 0; i < 8; i++) {
+            const response = await fetch(`https://dog.ceo/api/breeds/image/random`); // Petición GET a la API
+            const data = await response.json(); // Transformamos la respuesta a un json.
+
+            const div = document.createElement('div');
+            div.classList.add('container-card-image');
+            div.innerHTML = `
+                <img src="${data.message}" alt="${data.message}"/>
+            `;
+            section.appendChild(div);
+        }
+    } catch (err) {
+
+    }
+}
+
+getImageRandomDog();
