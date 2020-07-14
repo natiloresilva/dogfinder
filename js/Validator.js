3d56f5e0e384f428eb05c1fc2fbe1fdaf35374fc
Nati <natalia.pupushi@gmail.com>
2020-07-14 15:04:25 -0300

    validator form implemented

--- js/Validator.js
+++ js/Validator.js
@@ -4,7 +4,7 @@ class Validator {
     constructor() {
         //mensajes predeterminados
         this.invalidEmailError = 'Enter a valid email';
-        this.emailExistError = 'This Email has already been registered';
+        this.emailExistsError = 'This Email has already been registered';
         this.passwordError = 'Enter a password of 6 or more characters';
         this.repeatPasswordError = 'Passwords do not match';
 
@@ -44,19 +44,67 @@ class Validator {
         }
 
         //validar que el email no está tomado (es único)
+        validateUniqueEmail = (newEmail) => {
+            const usersDB = db.getAllUsers();
+
+            let emailUnique = true;
+
+            if(usersDB.length > 0) {
+                usersDB.forEach( (userObj) => {
+                    //si el email ya está tomado, cambia el valor de la variable a false
+                    if (userObj.email === newEmail) {
+                        emailUnique = false;
+                    }
+                })
+
+                if (emailUnique) {
+                    //quita el mensaje de error
+                    delete this.errors.emailExistsError;
+                }
+                else {
+                    //si el email no es unico, poner msj de nvo
+                    this.errors.emailExistsError = this.emailExistsError
+                }
+
+            }
+        }
         //validar la longitud del password
-        validatePassword = (password) => {}
+        validatePassword = (password) => {
+            if (password.length >= 6) {
+                //quita el msj de error
+                delete this.errors.passwordError;
+            }
+            else {
+                //sino muestra el msj de error
+                this.errors.passwordError = this.passwordError;
+            }
+        }
 
         //validar si el password y el repeat-password coinciden
-        validatePasswordRepeat = (password) => {}
+        validatePasswordRepeat = (password, passwordRepeat) => {
+            if (password === passwordRepeat) {
+                //si las 2 password coinciden borra el error
+                delete this.errors.repeatPasswordError;
+            }
+            else {
+                //sino pone el msj
+                this.errors.repeatPasswordError = this.repeatPasswordError;
+            }
+        }
 
         //Obtener el obj con el error, para mostrar al usuario en la pág. Signin
         getErrors = () => {
             return this.errors;
         }
 
-        //reiniciar los errores mostrados
-        resetValidator = () => {}
+        //reiniciar los errores mostrados, para el próximo Singup
+        resetValidator = () => {
+            this.errors = {
+                invalidEmailError: this.invalidEmailError,
+                passwordError: this.passwordError,
+                repeatPasswordError: this.repeatPasswordError
+            } 
+        }
 }
 
 const validator = new Validator();
--- js/signin.js
+++ js/signin.js
@@ -1,6 +1,6 @@
 'use strict';
 
-class Singup {
+class Signup {
     constructor() {
         this.nameInput = document.querySelector("#name");
         this.emailInput = document.querySelector("#email");
@@ -17,22 +17,45 @@ class Singup {
     handleEmailInput = (event) => {
         const email = event.target.value;
 
-        console.log('email', email);
+        //validar el texto del input email
+        validator.validateValidEmail(email);
+
+        const errors = validator.getErrors();
+
+        // si el nombre del email es válido
+
+        if (!errors.invalidEmailError) {
+            //comprueba si el email es único
+            validator.validateUniqueEmail(email);
+        }
+            this.setErrorMessages();
     }
 
 
 
     handlePasswordInput = (event) => {
         const password = event.target.value;
+        const passwordRepeat = this.repeatPasswordInput.value;
 
-        console.log('password', password);
+
+        //validar el texto del input password
+        validator.validatePassword(password);
+        validator.validatePasswordRepeat(password, passwordRepeat);
+
+            this.setErrorMessages();
     }
 
 
     handleRepeatPasswordInput = (event) => {
-        const repeatPassword = event.target.value;
+        const passwordRepeat = event.target.value;
+        const password = this.passwordInput.value;
+        
 
-        console.log('repeatPassword', repeatPassword);
+        //validar el texto del input password
+        validator.validatePassword(password);
+        validator.validatePasswordRepeat(password, passwordRepeat);
+
+            this.setErrorMessages();
     }
 
 
@@ -47,7 +70,7 @@ class Singup {
         const repeatPassword = this.repeatPasswordInput.value;
 
 
-        const newUser = new User(name, email, password,);
+        const newUser = new User(name, email, password);
 
         //guardamos el nuevo usuario en la base de datos simulada
 
@@ -67,12 +90,29 @@ class Singup {
     addListeners = () => {
         this.emailInput.addEventListener("input", this.handleEmailInput);
         this.passwordInput.addEventListener("input", this.handlePasswordInput);
-        this.repeatPasswordInput.addEventListener("input", this.handleRepeatPasswordInput)
+        this.repeatPasswordInput.addEventListener("input", this.handleRepeatPasswordInput);
 
-        this.buttonInput.addEventListener("click", this.saveData)
+        this.buttonInput.addEventListener("click", this.saveData);
+    }
+
+    setErrorMessages = () => {
+        //vacia los errores para que no se sumen
+        this.errorWrapper.innerHTML = "";
+        
+        const errorsObj = validator.getErrors();
+        
+        //convertir el objeto a un array de strings
+        const errorsStringsArr = Object.values(errorsObj);
+
+        errorsStringsArr.forEach( (errorStr) => {
+        const errorMessageP = document.createElement('p');
+        errorMessageP.innerHTML = errorStr;
+
+        this.errorWrapper.appendChild(errorMessageP);
+        })
     }
 }
 
-const signup = new Singup();
+const signup = new Signup();
 
 window.addEventListener("load", signup.addListeners);
