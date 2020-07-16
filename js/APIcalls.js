a020e0e2e2570075df8f72d9913e21626d25ff61
Nati <natalia.pupushi@gmail.com>
2020-07-16 08:18:55 -0300

    connect whith the api to get the names of the dogs

--- js/APIcalls.js
+++ js/APIcalls.js
@@ -4,15 +4,19 @@ async function getImageRandomDog() {
     const section = document.querySelector(".dog-list");
 
     try {
-        for (let i = 0; i < 8; i++) {
-            const response = await fetch(`https://dog.ceo/api/breeds/image/random`); // Petición GET a la API
-            const data = await response.json(); // Transformamos la respuesta a un json.
+        for (let i = 1; i <= 8; i++) {
+            const responseImage = await fetch(`https://dog.ceo/api/breeds/image/random`); // Petición GET a la API
+            const dataImage = await responseImage.json(); // Transformamos la respuesta a un json.
+
+            const responseName = await fetch(`https://jsonplaceholder.typicode.com/users/${i}`); // Petición GET a la API
+            const dataName = await responseName.json(); // Transformamos la respuesta a un json.
+            const nameDog = dataName.name.split(" ")[1];
 
             const div = document.createElement('div');
             div.classList.add('container-card-image');
             div.innerHTML = `
-                <img src="${data.message}" alt="${data.message}"/>
-                <p>Test<p/>
+                <img src="${dataImage.message}" alt="${dataImage.message}"/>
+                <p>${nameDog}<p/>
             `;
             section.appendChild(div);
         }
